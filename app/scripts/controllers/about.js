2cc58f914d120a5482b8be964d15161e763a8199
Todd Warren <todd@warrenfamily.org>
2015-02-14 17:39:37 -0800

    initial commit after doing yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name toddwincStrapApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the toddwincStrapApp
+ */
+angular.module('toddwincStrapApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name toddwincStrapApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the toddwincStrapApp
+ */
+angular.module('toddwincStrapApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
