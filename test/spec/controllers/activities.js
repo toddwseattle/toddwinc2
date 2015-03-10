73f121ac7cef54452e9f76a121db27425f4791b6
Todd Warren <twarren@ashesi.edu.gh>
2015-03-09 21:46:11 -0700

    fixed startups controller test with mock

--- test/spec/controllers/activities.js
+++ test/spec/controllers/activities.js
@@ -1,4 +1,4 @@
-'use strict';
+//'use strict';
 
 describe('Controller: ActivitiesCtrl', function () {
 
@@ -9,14 +9,23 @@ describe('Controller: ActivitiesCtrl', function () {
     scope;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope,$httpBackend,$http) {
     scope = $rootScope.$new();
+    httpMock= $httpBackend;
+    http=$http;
+
+    httpMock.when('GET','/data/activities.json').respond('[{"id" : 1,"activetype" : "activity","name" : "company name"}');
     ActivitiesCtrl = $controller('ActivitiesCtrl', {
-      $scope: scope
+      $scope: scope, $http: http
     });
   }));
 
   it('should attach a list of awesomeThings to the scope', function () {
     expect(scope.awesomeThings.length).toBe(3);
   });
+  it('should have a list of startups',function(){{
+    httpMock.expectGET('/data/activities.json');
+    httpMock.flush();
+    expect(scope.startups.length).toBeGreaterThan(0);
+  }});
 });
